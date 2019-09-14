1ce0433e6d176e974ceb9efcfc5af48a67dd8f8b
Kasper Karelse <kasperkarelse98@gmail.com>
2019-09-14 12:24:12 +0200

    get icon working

--- addon/back-end/index.js
+++ addon/back-end/index.js
@@ -1,10 +1,9 @@
 const bodyParser = require("body-parser");
 const express = require("express");
-const webpack = require('webpack');
-const https = require('https')
-const path = require('path');
-const officeDevCerts = require('office-addin-dev-certs');
-const webpackConfig = require('../webpack.config');
+const https = require("https");
+const officeDevCerts = require("office-addin-dev-certs");
+const path = require("path");
+const serveStatic = require('serve-static')
 
 const start = async () => {
   const app = express();
@@ -15,10 +14,15 @@ const start = async () => {
   await officeDevCerts.ensureCertificatesAreInstalled();
   const ssl = await officeDevCerts.getHttpsServerOptions();
 
-  const server = https.createServer({
-    key: ssl.key.toString(),
-    cert: ssl.cert.toString()
-  }, app)
+  const server = await https.createServer(
+    {
+      key: ssl.key.toString(),
+      cert: ssl.cert.toString()
+    },
+    app
+  );
+
+  app.use("/assets", express.static(path.join(__dirname, 'public')));
 
   app.get("/api/test", (req, res) => {
     console.log("/api/test");
