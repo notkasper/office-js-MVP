c9e8d0da90b38b5eb2a7a9cc657e737e7fbb007f
Kasper Karelse <kasperkarelse98@gmail.com>
2019-09-14 11:06:23 +0200

    fix

--- addon/front-end/components/DialogButton.jsx
+++ addon/front-end/components/DialogButton.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import { PrimaryButton } from "office-ui-fabric-react";
+import { PrimaryButton, Stack } from "office-ui-fabric-react";
 
 const DIALOG_HOST = "https://localhost:8081";
 
 export default class DialogButton extends React.Component {
-  openDialog = () => {
+  openDialog = dialogName => {
     console.log("Opening dialog...");
     Office.context.ui.displayDialogAsync(
-      DIALOG_HOST,
+      `${DIALOG_HOST}/${dialogName}`,
       { height: 64, width: 64, displayInIframe: true },
       result => {
         console.log(`Dialog open result: ${JSON.stringify(result)}`);
@@ -15,7 +15,20 @@ export default class DialogButton extends React.Component {
     );
   };
 
+  openForm1 = () => {
+    this.openDialog("user_form_1");
+  };
+
+  openForm2 = () => {
+    this.openDialog("user_form_2");
+  };
+
   render() {
-    return <PrimaryButton onClick={this.openDialog}>Open Dialog</PrimaryButton>;
+    return (
+      <Stack horizontal>
+        <PrimaryButton onClick={this.openForm1}>Open Form1</PrimaryButton>
+        <PrimaryButton onClick={this.openForm2}>Open Form2</PrimaryButton>
+      </Stack>
+    );
   }
 }
