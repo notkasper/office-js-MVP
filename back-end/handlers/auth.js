0f03a9a72ff06309cfd71cbdc4c08227c6c41d15
Kasper Karelse <kasperkarelse98@gmail.com>
2019-09-28 15:07:50 +0200

    getting somewhere

--- back-end/handlers/auth.js
+++ back-end/handlers/auth.js
@@ -2,16 +2,16 @@ const { AuthenticationContext } = require("adal-node");
 const crypto = require("crypto");
 
 const config = {
-  tenant: "",
+  tenant: "0abccceb-93ba-4767-9038-76722263a6ee",
   authorityHostUrl: "https://login.windows.net",
-  clientId: "",
+  clientId: "f8e43d80-b4ca-4322-b6c7-84e39742e4d4",
   clientSecret: ""
 };
 
-const authorityUrl = `${config.AuthenticationContext}/${config.tenant}`;
+const authorityUrl = `${config.authorityHostUrl}/${config.tenant}`;
 // WARNING: GET THIS FROM CONFIG IN PROD AND USE NGROK
 const redirectUri = "https://localhost:3000/getAccessToken";
-const resource = "00000002-0000-0000-c000-000000000000";
+const resource = "https://graph.microsoft.com/";
 
 const templateAuthzUrl = `https://login.windows.net/${config.tenant}/oauth2/authorize?response_type=code&client_id=<client_id>&redirect_uri=<redirect_uri>&state=<state>&resource=<resource>`;
 
@@ -36,32 +36,44 @@ const start = (req, res) => {
     res.cookie("authstate", token);
     const authorizationUrl = createAuthorizationUrl(token);
 
+    console.log(authorizationUrl)
     res.redirect(authorizationUrl);
-  });
+});
 };
 
 const handle = (req, res) => {
   if (req.cookies.authstate !== req.query.state) {
-    res.status(500).send('error: state does not match');
+    res.status(500).send("error: state does not match");
     return;
   }
   const authenticationContext = new AuthenticationContext(authorityUrl);
-  authenticationContext.acquireTokenWithAuthorizationCode(req.query.code, redirectUri, resource, sampleParameters.clientId, sampleParameters.clientSecret, (error, response) => {
-    if (error) {
-      const message = `error: ${error.message}\nresponse: ${JSON.stringify(response)}`;
-      res.status(500).send(message);
-      return;
-    }
-    // Later, if the access token is expired it can be refreshed.
-    // authenticationContext.acquireTokenWithRefreshToken(response.refreshToken, sampleParameters.clientId, sampleParameters.clientSecret, resource, function(refreshErr, refreshResponse) {
-    //   if (refreshErr) {
-    //     message += 'refreshError: ' + refreshErr.message + '\n';
-    //   }
-    //   message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
+  console.log("HERE!");
+  authenticationContext.acquireTokenWithAuthorizationCode(
+    req.query.code,
+    redirectUri,
+    resource,
+    sampleParameters.clientId,
+    sampleParameters.clientSecret,
+    (error, response) => {
+      console.log("HERE2");
+      if (error) {
+        const message = `error: ${error.message}\nresponse: ${JSON.stringify(
+          response
+        )}`;
+        res.status(500).send(message);
+        return;
+      }
+      // Later, if the access token is expired it can be refreshed.
+      // authenticationContext.acquireTokenWithRefreshToken(response.refreshToken, sampleParameters.clientId, sampleParameters.clientSecret, resource, function(refreshErr, refreshResponse) {
+      //   if (refreshErr) {
+      //     message += 'refreshError: ' + refreshErr.message + '\n';
+      //   }
+      //   message += 'refreshResponse: ' + JSON.stringify(refreshResponse);
 
-    //   res.send(message); 
-    // }); 
-  });
+      //   res.send(message);
+      // });
+    }
+  );
 };
 
 module.exports = {
--- back-end/routes/api.js
+++ back-end/routes/api.js
@@ -9,27 +9,6 @@ router.get("/test", (req, res) => {
   res.status(200).send({ message: "OK" });
 });
 
-router.post("/signin", (req, res) => {
-  let accounts;
-  try {
-    accounts = JSON.parse(
-      fs.readFileSync(path.join(__dirname, "../../accounts.json"))
-    );
-  } catch (error) {
-    throw new Error(`Error while reading accounts from file: ${error}`);
-  }
-  const username = req.body.username;
-  if (!username) {
-    res.status(400).send({ message: "Please provide a username" });
-  }
-  const user = accounts[username];
-  if (req.body.password === user.password) {
-    res.status(200).send();
-    return;
-  }
-  res.status(400).send({ message: "Wrong log in" });
-});
-
 router.put("/dialog", (req, res) => {
   res.status(201).send({ message: "CREATED", body: req.body });
 });
