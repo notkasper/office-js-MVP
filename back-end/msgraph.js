7a7ea1688a4fa36367fb19f42742746f668fce01
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-03 21:28:59 +0200

    refactor

---
+++ back-end/msgraph.js
@@ -0,0 +1,34 @@
+require("isomorphic-fetch");
+const { Client } = require("@microsoft/microsoft-graph-client");
+
+const getUserDetails = async accessToken => {
+  const authProvider = {
+    getAccessToken: () => {
+      return accessToken;
+    }
+  };
+
+  const options = {
+    authProvider
+  };
+
+  const client = Client.initWithMiddleware(options);
+
+  let userDetails;
+  try {
+    userDetails = await client.api("/me").get();
+  } catch (error) {
+    console.error(
+      `Error while requesting profile information: ${JSON.stringify(error)}`
+    );
+    throw new Error(
+      "Er is iets fout gegaan bij het ophalen van het profiel, probeer het later opnieuw of neem contact op met support."
+    );
+  }
+
+  return userDetails;
+};
+
+module.exports = {
+  getUserDetails
+};
