4188fd8156c93b254166863511d00cc9777f3891
Kasper Karelse <kasperkarelse98@gmail.com>
2019-10-16 23:34:04 +0200

    hotfix 5

--- back-end/server.js
+++ back-end/server.js
@@ -3,7 +3,6 @@ const http = require("http");
 const db = require("./db");
 const app = require("./app");
 const { getSslCert, getSslKey, getEnv } = require("../utils");
-require("../seed");
 
 const normalizePort = val => {
   const port = parseInt(val, 10);
@@ -56,7 +55,7 @@ if (env === "development") {
   server = http.createServer(app);
 }
 
-// db.connect();
+db.connect();
 
 server.listen(port);
 server.on("error", onError);
