ed14c9c2bdfe69be995c6524a6b9d490a9ca14c7
Kasper <kasperkarelse98@gmail.com>
2019-10-29 19:45:23 +0100

    add auth middleware to getme endpoint

--- back-end/routes/auth.js
+++ back-end/routes/auth.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const { getAuthorizationUrl, acquireTokenWithAuthorizationCode, getUserDetails } = require('../controllers/auth');
+const authMiddleware = require('../middleware/auth');
 
 const router = express.Router();
 
 router.get('/url', getAuthorizationUrl);
 router.get('/token', acquireTokenWithAuthorizationCode);
-router.get('/me', getUserDetails);
+router.get('/me', authMiddleware, getUserDetails);
 
 module.exports = router;
