a8e4cb8e1671634bf9131ccdf02509fef884ee99
Kasper Karelse <kasperkarelse98@gmail.com>
2019-11-25 21:01:18 +0100

    log env

--- back-end/server.js
+++ back-end/server.js
@@ -45,7 +45,7 @@ const onError = error => {
 const onListening = server => {
   const addr = server.address();
   const bind = typeof addr === 'string' ? `pipe: ${addr}` : `port: ${addr.port}`;
-  console.log(`Listening on: ${bind}`);
+  console.log(`Listening on: ${bind} with env: ${process.env}`);
 };
 
 const start = async () => {
